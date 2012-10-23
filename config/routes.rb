7e7ab7e3b547d83406ef39912d226072e0735f6a
Gabriel Kho <gabriel.d.kho@gmail.com>
2012-10-22 21:56:23 -0700

    Question changing

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Stealthmode::Application.routes.draw do
   match "/questions/main" => "questions#main"
   match "/questions/add" => "questions#add"
   match "/questions/language" => "questions#language"
-  match "/questions/language" => "questions#change"
+  match "/questions/change" => "questions#change"
   match "/questions/edit" => "questions#edit"
   match "/questions/delete" => "questions#delete"
   match "/questions/save" => "questions#save"
