ff2f00b02bd545c8da1d665096c3531b44e2163a
Gabriel Kho <gabriel.d.kho@gmail.com>
2012-11-26 13:09:38 -0800

    Yeah

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,7 @@
 class LoginController < ApplicationController
   def main
   	patient = Patient.new
-  	patient.start = Time.now.to_i
+  	#patient.start = Time.now.to_i
   	patient.answers = ""
   	patient.save
   	@patient_id = patient.id
