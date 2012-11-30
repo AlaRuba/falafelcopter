683378ba0689cd0d4782882d5d950571aec981e2
Gabriel Kho <gabriel.d.kho@gmail.com>
2012-11-30 11:39:49 -0800

    Fixed

--- db/migrate/20121130192829_add_time_to_sessions.rb
+++
@@ -1,6 +0,0 @@
-class AddTimeToSessions < ActiveRecord::Migration
-  def change
-    add_column :sessions, :start, :integer
-    add_column :sessions, :end, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126183413) do
+ActiveRecord::Schema.define(:version => 20121130192829) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question"
@@ -69,8 +69,8 @@ ActiveRecord::Schema.define(:version => 20121126183413) do
     t.text     "data"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.datetime "start_time"
-    t.datetime "end"
+    t.integer  "start"
+    t.integer  "end"
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
