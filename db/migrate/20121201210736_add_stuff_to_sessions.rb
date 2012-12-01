6775f05510a260bc653e8575d653734fb189a81d
Gabriel Kho <gabriel.d.kho@gmail.com>
2012-12-01 13:09:37 -0800

    Final

--- db/migrate/20121201210736_add_stuff_to_sessions.rb
+++ db/migrate/20121201210736_add_stuff_to_sessions.rb
@@ -1,6 +1,6 @@
 class AddStuffToSessions < ActiveRecord::Migration
   def change
     add_column :sessions, :entry, :integer
-    add_column :sessions, :end, :integer
+    add_column :sessions, :exit, :integer
   end
 end
