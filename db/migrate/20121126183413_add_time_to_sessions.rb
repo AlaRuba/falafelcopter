8398552d539ee6799419c2f6f9a984f10e0dfab1
Gabriel Kho <gabriel.d.kho@gmail.com>
2012-12-01 12:50:54 -0800

    File deleted

--- db/migrate/20121126183413_add_time_to_sessions.rb
+++
@@ -1,6 +0,0 @@
-class AddTimeToSessions < ActiveRecord::Migration
-  def change
-    add_column :sessions, :start_time, :datetime
-    add_column :sessions, :end, :datetime
-  end
-end
--- db/migrate/20121126210712_add_time_to_sessions.rb
+++
@@ -1,6 +0,0 @@
-class AddTimeToSessions < ActiveRecord::Migration
-  def change
-    add_column :sessions, :start, :integer
-    add_column :sessions, :finish, :integer
-  end
-end
